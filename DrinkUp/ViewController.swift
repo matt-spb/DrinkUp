56020d125e2f426f9782b6a605b3463a583a1e91
Matvey <mnf89@mail.ru>
2019-12-14 11:12:27 +0300

    nikita done my work

--- DrinkUp/ViewController.swift
+++ DrinkUp/ViewController.swift
@@ -12,9 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputTextFiled.delegate = self
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func backInitialVC (_ segue: UIStoryboardSegue) {}
 
-
+    @IBOutlet weak var inputTextFiled: UITextField!
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let nextController = segue.destination as? ViewController2 else { return }
+        
+        let inputString = inputTextFiled.text ?? "нихуя"
+       // let randomString = UUID().uuidString
+        
+        // путь пидора
+        //nextController.loadViewIfNeeded() //принудительно загружаем вью
+        //nextController.label.text = randomString
+        
+        //норм путь
+        //nextController.labelText = randomString
+        nextController.labelText = inputString
+    }
 }
 
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //вызывается при нажатии на энтер
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
